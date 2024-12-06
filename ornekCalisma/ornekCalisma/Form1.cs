31fe515800ba298f385317eb2125d3ba9dc773da
Rukiye <fotorukiyeb1544@gmail.com>
2024-12-06 15:52:52 +0300

    Add files via upload

---
+++ ornekCalisma/ornekCalisma/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace ornekCalisma
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+            metot(true); 
+        }
+
+        
+        private void metot(bool value)
+        {
+            if (value)
+            {
+                label1.Text = "Sayı Asaldır."; 
+            }
+            else
+            {
+                label1.Text = "Sayı Asal Değildir."; 
+            }
+        }
+    }
+}
+
